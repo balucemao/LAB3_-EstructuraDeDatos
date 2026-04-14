4087380826fce4d09979728e8172657fbf5f26e3
Your Name <you@example.com>
2026-04-14 23:35:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -72,7 +72,7 @@ HashMap * createMap(long capacity) {
 
 void insertMap(HashMap * map, char * key, void * value) {
     //primero revisar si el elemento es correcto
-    if (map == NULL || key == NULL)return NULL;
+    if (map == NULL || key == NULL)return;
 }
 
 // 3. Implemente la función Pair * searchMap(HashMap * map, char * key), la cual retorna el Pair asociado a la clave ingresada. 
