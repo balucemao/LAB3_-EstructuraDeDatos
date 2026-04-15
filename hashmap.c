894d00f00f7230c1d4ec62d2e39f11de9e8e9d7a
Your Name <you@example.com>
2026-04-15 00:01:43 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -87,7 +87,7 @@ void insertMap(HashMap * map, char * key, void * value) {
         map->buckets[posicion]->value = value;
     }
 
-    createPair(map->buckets[posicion]->key, value);
+    map->buckets[posicion] = createPair(key, value);
 }
 
 // 3. Implemente la función Pair * searchMap(HashMap * map, char * key), la cual retorna el Pair asociado a la clave ingresada. 
