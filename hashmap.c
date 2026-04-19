2bb80e21e20de9bf0833391d88f96090e7277049
Your Name <you@example.com>
2026-04-19 13:31:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -106,9 +106,9 @@ Pair * searchMap(HashMap * map,  char * key) {
     if (map == NULL || key == NULL)return NULL;
     long posicion = hash(key, map->capacity);
 
-    //se recorre hasta encontrar la clave o casilla nula
+    //se recorre hasta encontrar la clave o casilla ny
     while(map->buckets[posicion] != NULL){  
-        if (is_equal(map->buckets[posicion]->key, key)){
+        if (map->buckets[posicion]->key != NULL && is_equal(map->buckets[posicion]->key, key)){
             map->current = posicion;
             return map->buckets[posicion];
         }
